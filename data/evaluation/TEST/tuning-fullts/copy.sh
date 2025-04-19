2cca371946bac1818dcdcabe8bd8c50864676d8c
JohnyDevp <holan.jan@outlook.com>
2025-04-19 17:35:31 +0200

    the rest

--- data/evaluation/TEST/tuning-fullts/copy.sh
+++ data/evaluation/TEST/tuning-fullts/copy.sh
@@ -4,13 +4,13 @@ ROOT=/mnt/scratch/tmp/xholan11
 NUMBER_OF_EXPERIMENTS=3
 
 # FULLTS
-DIR=models/planned-allds-fullts/30/weight_decay       # CHANGE THIS LINE
-EXPER_NAME_DIRS=wd                             # CHANGE THIS LINE
-DST=./allds/30/weight_decay                           # CHANGE THIS LINE
+DIR=models/planned-allds-fullts/30/epochs       # CHANGE THIS LINE
+EXPER_NAME_DIRS=ep                             # CHANGE THIS LINE
+DST=./allds/30/epochs                           # CHANGE THIS LINE
 # #======================================================
-# DIR=models/planned-vanmed-fullts/weight_decay        # CHANGE THIS LINE
-# EXPER_NAME_DIRS=wd                                # CHANGE THIS LINE
-# DST=./vanmed/30/weight_decay                           # CHANGE THIS LINE
+# DIR=models/planned-vanmed-fullts/epochs        # CHANGE THIS LINE
+# EXPER_NAME_DIRS=ep                                # CHANGE THIS LINE
+# DST=./vanmed/30/epochs                           # CHANGE THIS LINE
 
 for i in $(seq 1 $NUMBER_OF_EXPERIMENTS); do
     scp $SERVER:$ROOT/$DIR/$EXPER_NAME_DIRS${i}/eval.txt $DST/$EXPER_NAME_DIRS${i}_eval.txt
