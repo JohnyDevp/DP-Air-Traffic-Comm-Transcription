11d555a24f10b1449e2c109ee6bcd91edd9d87a2
JohnyDevp <holan.jan@outlook.com>
2025-04-23 09:07:06 +0200

    rest

--- data/evaluation/TEST/tuning-shortts/copy.sh
+++ data/evaluation/TEST/tuning-shortts/copy.sh
@@ -5,17 +5,19 @@ NUMBER_OF_EXPERIMENTS=3
 
 # SHORTTS
 # ALLDS
-# DIR=models/planned-allds-shortts/epochs       # CHANGE THIS LINE
-# EXPER_NAME_DIRS=ep                              # CHANGE THIS LINE
-# DST=./allds/epochs                           # CHANGE THIS LINE
+DIR=models/planned-allds-shortts/epochs       # CHANGE THIS LINE
+EXPER_NAME_DIRS=ep                              # CHANGE THIS LINE
+DST=./allds/epochs                           # CHANGE THIS LINE
 # ======================================================
 # VANMED
-DIR=models/planned-vanmed-shortts/learning_rate        # CHANGE THIS LINE
-EXPER_NAME_DIRS=lr                                # CHANGE THIS LINE
-DST=./vanmed/learning_rate                           # CHANGE THIS LINE
+# DIR=models/planned-vanmed-shortts/learning_rate        # CHANGE THIS LINE
+# EXPER_NAME_DIRS=lr                                # CHANGE THIS LINE
+# DST=./vanmed/learning_rate                           # CHANGE THIS LINE
 
 for i in $(seq 1 $NUMBER_OF_EXPERIMENTS); do
     scp $SERVER:$ROOT/$DIR/$EXPER_NAME_DIRS${i}/eval.txt $DST/$EXPER_NAME_DIRS${i}_eval.txt
     scp $SERVER:$ROOT/$DIR/$EXPER_NAME_DIRS${i}/training_details.txt $DST/$EXPER_NAME_DIRS${i}_td.txt
     echo "DONE copying $SERVER:$ROOT/$DIR/$EXPER_NAME_DIRS${i}/{eval.txt, training_details.txt} to $DST"
 done
+
+# scp merlin:$ROOT/models/planned-allds-shortts/defpar/*.txt ./allds/defpar/
