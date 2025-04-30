5f62a0ff7ff576225f3443afcc88d6ce8c15e489
JohnyDevp <holan.jan@outlook.com>
2025-04-30 16:17:38 +0200

    shuffle prompt + restructured

--- scripts/wer_proportion.py
+++ scripts/wer_proportion.py
@@ -1,5 +1,6 @@
 import os
 import re, json
+import sys
 from bs4 import BeautifulSoup, ResultSet
 
 
@@ -24,12 +25,17 @@ for file in meta_files:
     weighted_percentage_total = 0.0
     weights_total = 0
     total_records =0
+    total_callsigns = 0
+    cal_write = open(file,'w')
+    
     for xml_file in xml_files:
-        if ('segidx' in xml_file and xml_file in portioned): continue
-        elif ('segidx' in xml_file and xml_file not in portioned):
+        if ('segidx' in xml_file and 'segidx0' not in xml_file): 
+            print(f'File {xml_file} already processed',file=sys.stderr)
+            continue
+        elif ('segidx0' in xml_file):
             xml_file_basename = os.path.basename(xml_file).split('.xml')[0]
             # build up the correct path to the current disk, where the file should be located
-            portioned.append(xml_file)
+            # portioned.append(xml_file_basename)
             for fl in FOLDERS_TO_SEARCH:
                 p = os.path.join(ROOT_DISK,fl,xml_file_basename+'.xml')
                 if os.path.exists(p):
@@ -42,19 +48,22 @@ for file in meta_files:
         bs=BeautifulSoup(xml_content,features='xml')
         full_ts_with_tags = ''    
         for segment in bs.find_all('segment'):
-            full_ts_with_tags += segment.find('text').text
+            full_ts_with_tags +=re.sub(r'\[\/#(\w+)\]\s*\[#\1\]',' ',segment.find('text').text)
         
         # now obtain all words in callsigns
         pattern= r'\[#callsign\](.*?)\[/#callsign\]'
         all_callsigns = re.findall(pattern, full_ts_with_tags)
+        total_callsigns += len(all_callsigns) 
         all_callsigns_chars_length = 0
         all_callsigns_word_length = 0
+        cal_write.write(f'{xml_file}\t{len(all_callsigns)}\n')
         for callsign in all_callsigns:
+            cal_write.write(f'\t{callsign}\n')
             # normalize callsign
             callsign = re.sub(r'\s+', ' ', callsign).strip()
             all_callsigns_chars_length += len(callsign)
             all_callsigns_word_length += len(callsign.split())
-            
+        cal_write.write('\n')
         # remove all callsigns tags with its content
         # full_ts_without_callsigns = re.sub(pattern, ' ', full_ts_with_tags)
         # now remove all other tags
@@ -81,6 +90,7 @@ for file in meta_files:
     print(f'File: {file}')
     print(f'Total records: {total_records}')
     print(f'Full TS without callsigns: {fullts_total_chars_length} chars, {fullts_total_word_length} words')
+    print(f'Total callsigns: {total_callsigns}')
     print(f'Calls signs: {callsigns_chars_length} chars, {callsigns_word_length} words')
     print(f'AVG Percentage of callsigns: {(percentage_total/len(xml_files)):.2f}%; Weighted: {(weighted_percentage_total/weights_total * 100):.2f}%')     
     print(f'Total percentage in chars: {callsigns_chars_length / float(fullts_total_chars_length) * 100.0:.2f}%')
